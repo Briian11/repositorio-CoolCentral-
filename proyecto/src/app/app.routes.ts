e6a67f854e6b5505936617b8b051f81fd4f536b8
Victor <victor.tardonballesteros@cieep.com>
2024-04-28 14:55:41 +0200

    Arreglando enrutamientos varios

--- proyecto/src/app/app.routes.ts
+++ proyecto/src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NabvarComponent } from './components/nabvar/nabvar.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
 export const routes: Routes = [
+    { path: '',   redirectTo: '/home', pathMatch: 'full' },
+    {path: 'home', component: HomeComponent},
+    {path: 'nabvar', component: NabvarComponent},
     { path: 'product', component: ProductComponent},
+    { path: 'cart', component: CartComponent},
+    {path: 'log-in', component: LogInComponent},
+    { path: 'footer', component: FooterComponent},
 ];
--- proyecto/src/app/components/home/home.component.ts
+++ proyecto/src/app/components/home/home.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive ],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
