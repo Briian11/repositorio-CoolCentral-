e699f79869e68a0ce81d0027ed62ac470e73c357
Victor <victor.tardonballesteros@cieep.com>
2024-05-03 11:01:41 +0200

    Aplicando lógica al selector de pago

--- proyecto/src/app/components/cart/cart.component.ts
+++ proyecto/src/app/components/cart/cart.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+selectedPaymentMethod: string;
+cardholderName: string;
+cardNumber: string;
+expirationDate: string;
+cvv: string;
+paypalUsername: string;
+paypalPassword: string;
+accountHolderName: string;
+accountNumber: string;
 
+constructor() {
+  this.selectedPaymentMethod = ''; // Inicializar si es necesario
+  this.cardholderName = '';
+  this.cardNumber = '';
+  this.expirationDate = '';
+  this.cvv = '';
+  this.paypalUsername = '';
+  this.paypalPassword = '';
+  this.accountHolderName = '';
+  this.accountNumber = '';
+}
 }
